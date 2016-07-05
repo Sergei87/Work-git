8b40748c4c468103ae97c49628140e6865086639
Sergei87 <zaytsevs87@gmail.com>
2016-07-05 21:26:59 +0300

    .gitignore was added. helloworld server was added

--- app.js
+++ app.js
@@ -3,4 +3,14 @@
 let express = require('express');
 let app = express();
 
+
+app.set('port', 3000);
+app.get('/', function (req, res) {
+	console.log("Express server listening on port " + app.get('port'));
+  	res.send('Hello World');
+})
+
+app.listen(3000);
+
 module.exports = app;
+
