51d94142e044571d3368229bb46df696d73ff90f
Sergei87 <zaytsevs87@gmail.com>
2016-07-06 09:54:19 +0300

    install npm jade, made template for form by jade

--- app.js
+++ app.js
@@ -3,14 +3,20 @@
 let express = require('express');
 let app = express();
 
+app.set('views', './views');
+ app.set('view engine', 'jade');
+
 
 app.set('port', 3000);
 app.get('/', function (req, res) {
 	console.log("Express server listening on port " + app.get('port'));
-  	res.send('Hello World');
+  	res.render('index', { title: 'Form', message: 'Hello there!'});
 })
 
 app.listen(3000);
+
+
+
 
 module.exports = app;
 
