b4a29ddde80d0fb658d60f63fa47a924af58b6ae
Sergei87 <zaytsevs87@gmail.com>
2016-07-06 20:59:07 +0300

    registration was added

--- app.js
+++ app.js
@@ -3,17 +3,74 @@
 let express = require('express');
 let app = express();
 
-app.set('views', './views');
- app.set('view engine', 'jade');
+let pgp = require("pg-promise")(/*options*/);
+let db = pgp("postgres://postgres:123@localhost:5432/test");
 
+let bodyParser = require('body-parser')
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+app.set('views', './views');
+app.set('view engine', 'jade');
 
 app.set('port', 3000);
 app.get('/', function (req, res) {
 	console.log("Express server listening on port " + app.get('port'));
-  	res.render('index', { title: 'Form', message: 'Hello there!'});
-})
+	
+});
 
+app.get('/register', function (req, res) {
+	console.log("Express server listening on port " + app.get('port'));
+	res.render('register', { title: 'Form', message: 'Hello there!'});
+});
+
+app.get('/login', function (req, res) {
+	console.log("Express server listening on port " + app.get('port'));
+	res.render('login', { title: 'Form', message: 'Hello there!'});
+});
 app.listen(3000);
+
+app.post('/api/register', function (req, res) {
+	console.log("/api/register");
+	console.log(req.body);
+	
+	if (!req.body.email) {
+		res.status(400).send('email isn\'t specified');
+	} else if (!req.body.password) {
+		res.status(400).send('password isn\'t specified');
+	} else {
+		db.any("SELECT email FROM users WHERE email = $1", req.body.email)
+			.then(function (emails) {
+			    if (emails.length == 0) {
+			    	db.query("INSERT INTO users (email, password, name) VALUES ($1, $2, $3)",
+			    		[req.body.email, req.body.password, req.body.username]);
+
+			    	db.any("SELECT * FROM users")
+						.then(function (users) {				
+						    console.log("users:", users);
+						});
+			    	console.log('new user has been added');
+			    	res.status(201).send('registration succeeded');
+			    } else {
+			    	res.status(400).send('email is used');
+			    }
+			})
+			.catch(function (error) {
+			    console.log("ERROR: ", error);
+			    res.status(401).send(error);
+			});
+	}
+
+} );
+
+app.post('/api/login', function (req, res) {
+	console.log("/api/login");
+	res.send("200 OK");
+} );
+
+/*
 let passport = require('passport')
 let LocalStrategy = require('passport-local').Strategy;
 
@@ -46,7 +103,7 @@ passport.use('local', new LocalStrategy(
         successRedirect: '/',
         failureRedirect: '/login',
         failureFlash: true })
-    ); 
+    ); */
 
 module.exports = app;
 
--- db.js
+++ db.js
@@ -1,14 +1,38 @@
 
 var pgp = require("pg-promise")(/*options*/);
-var db = pgp("postgres://postgres:123@localhost:5432/db");
+var db = pgp("postgres://postgres:123@localhost:5432/test");
 
+//db.query("CREATE DATABASE test");
+//db.query("CREATE TABLE users");
+/*
 db.one("SELECT $1 AS value", 123)
     .then(function (data) {
         console.log("DATA:", data.value);
     })
     .catch(function (error) {
-        console.log("ERROR:", error);
-    });
+        console.log("ERROR: !!!!!!!!!!!!!!!!!!!!!!1", error);
+    });*/
+/*db.query("INSERT INTO users (email, password, name) VALUES ('$1', '$2', '$3')",
+			    		['qwe', 'rty', 'yui']);*/
+
+
+/*db.any("INSERT INTO users (email, password, name) VALUES ($1, $2, $3)",
+			    		['qwe', 'rty', 'yui'])
+.then(function (users) {
+    console.log("email:", users);
+})
+.catch(function (error) {
+    console.log("ERROR: ", error);
+});*/
+
+
+db.any("SELECT email FROM users WHERE email = $1", 'sddsdsds')
+.then(function (users) {
+    console.log("email:", users);
+})
+.catch(function (error) {
+    console.log("ERROR: ", error);
+});
 
 
  module.exports = db
